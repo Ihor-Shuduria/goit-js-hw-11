68b9dbfa47c7a3f85673a6916f7c335223e6c411
igor <igorshudurya@gmail.gom>
2025-08-13 18:14:45 +0200

    оновив код

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -11,13 +11,21 @@ const lightbox = new SimpleLightbox('.gallery a', {
 
 export function createGallery(images) {
   const markup = images
-    .map(
-      image => `
-        <a href="${image.largeImageURL}">
-          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy"/>
-        </a>
-      `
-    )
+    .map(img => {
+      return `
+        <li class="gallery-item">
+          <a href="${img.largeImageURL}">
+            <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+          </a>
+          <div class="info">
+            <p><b>Likes:</b> ${img.likes}</p>
+            <p><b>Views:</b> ${img.views}</p>
+            <p><b>Comments:</b> ${img.comments}</p>
+            <p><b>Downloads:</b> ${img.downloads}</p>
+          </div>
+        </li>
+      `;
+    })
     .join('');
 
   galleryContainer.insertAdjacentHTML('beforeend', markup);
--- src/main.js
+++ src/main.js
@@ -8,17 +8,16 @@ import {
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-const form = document.querySelector('.form');
+const formEl = document.querySelector('.form');
 
-form.addEventListener('submit', async event => {
-  event.preventDefault();
-  const query = event.target.elements['search-text'].value.trim();
+formEl.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const query = e.target.elements['search-text'].value.trim();
 
   if (!query) {
-    iziToast.error({
-      title: 'Error',
+    iziToast.warning({
       message: 'Please enter a search term!',
-      position: 'topRight',
     });
     return;
   }
@@ -30,21 +29,16 @@ form.addEventListener('submit', async event => {
     const data = await getImagesByQuery(query);
 
     if (data.hits.length === 0) {
-      iziToast.warning({
-        title: 'No Results',
+      iziToast.error({
         message:
           'Sorry, there are no images matching your search query. Please try again!',
-        position: 'topRight',
       });
-      return;
+    } else {
+      createGallery(data.hits);
     }
-
-    createGallery(data.hits);
   } catch (error) {
     iziToast.error({
-      title: 'Error',
       message: 'Something went wrong. Please try again later.',
-      position: 'topRight',
     });
   } finally {
     hideLoader();
